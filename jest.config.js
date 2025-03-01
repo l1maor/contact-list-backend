0b7bf505eb7bd3c079b776b6c50e3a5ae2fdfaaa
l1maor <lim4or@gmail.com>
2025-03-01 07:46:01 -0500

    added initial project setup with TypeScript and Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  rootDir: ".",
+  testMatch: ["<rootDir>/tests/**/*.test.ts"],
+  moduleFileExtensions: ["ts", "js"],
+  coverageDirectory: "./coverage",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.test.json",
+      },
+    ],
+  },
+  setupFilesAfterEnv: ['<rootDir>/tests/singleton.ts'],
+};
